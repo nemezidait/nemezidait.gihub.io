097ae8252a51d3198b2fbd09cab3519f0df37c27
nemezidait <90568128+nemezidait@users.noreply.github.com>
2021-09-19 16:29:08 +0300

    Update cpplesson.js

--- js/cpplesson.js
+++ js/cpplesson.js
@@ -2,11 +2,25 @@
 
 var editor = null, diffEditor = null;
 
+function initializeFileHeaders(mainFile, additionalFiles){
+    let htmls = '<input type="button" id="' + getFileButtonId(mainFile) + '" class="btn btn-dark" value="'+ mainFile +'" />';
+    additionalFiles.forEach((name, i) =>{
+        htmls += '<input type="button" id="' + getFileButtonId(name) + '" class="btn btn-light" value="'+ name +'" />'
+    });
+    
+    let codeFiles = document.getElementById('codeFiles');
+    codeFiles.innerHTML = htmls;
+}
+
 function getLessonSettings(){
     let request = new XMLHttpRequest();
     request.open("GET", "config.json", false);
     request.send(null);
     return JSON.parse(request.responseText);
+}
+
+function getFileButtonId(fileName){
+ return fileName.replaceAll('.', '_');
 }
 
 function getStorageName(settings){
@@ -76,47 +90,27 @@ function onFileLoadError(err){
         $('#editor').append('<p class="alert alert-error">Failed to load ' + languageCode + ' sample</p>');  
 }
 
-function compile(){
-       
+function loadSample(languageCode, code) {
+    if (!editor) {
+        $('#editor').empty();
+        editor = monaco.editor.create(document.getElementById('editor'), {
+            model: null,
+        });
+    }
+
+    var oldModel = editor.getModel();
+    var newModel = monaco.editor.createModel(code, languageCode);
+    editor.setModel(newModel);
+    if (oldModel) {
+        oldModel.dispose();
+    }
+    changeTheme(0);
 }
 
-$(document).ready(function () {
-    const settings = getLessonSettings();
-
-    const languageMode = 'cpp';
-        
-    const storageName = getStorageName(settings);
-    const savedCodeJson = localStorage.getItem(storageName);
-        
-    if (savedCodeJson) {
-        // saved code from db
-        const savedCode = JSON.parse(savedCodeJson);
-        $('.loading.editor').show();
-        loadSample(languageMode, savedCode.mainFile.code);
-        $('.loading.editor').fadeOut({ duration: 300 });
-    }
-    else {
-        // defaul sample
-        initStorageWithTemplateCode(
-            settings,
-            () => {
-                const savedCode = JSON.parse(localStorage.getItem(storageName));
-                $('.loading.editor').show();
-                console.log(savedCode);
-                loadSample(languageMode, savedCode.mainFile.code);
-                $('.loading.editor').fadeOut({ duration: 300 });
-            });
-    }
-
-    window.onresize = function () {
-        if (editor) {
-            editor.layout();
-        }
-        if (diffEditor) {
-            diffEditor.layout();
-        }
-    };
-});
+function changeTheme(theme) {
+    var newTheme = (theme === 0 ? 'vs-dark' : 'vs');
+    monaco.editor.setTheme(newTheme);
+}
 
 var preloaded = {};
 (function () {
@@ -145,24 +139,45 @@ function xhr(url, cb) {
     });
 }
 
-function loadSample(languageCode, code) {
-    if (!editor) {
-        $('#editor').empty();
-        editor = monaco.editor.create(document.getElementById('editor'), {
-            model: null,
-        });
-    }
-
-    var oldModel = editor.getModel();
-    var newModel = monaco.editor.createModel(code, languageCode);
-    editor.setModel(newModel);
-    if (oldModel) {
-        oldModel.dispose();
-    }
-    changeTheme(0);
+function compile(){
+       
 }
 
-function changeTheme(theme) {
-    var newTheme = (theme === 0 ? 'vs-dark' : 'vs');
-    monaco.editor.setTheme(newTheme);
-}
+$(document).ready(function () {
+    const settings = getLessonSettings();
+    const storageName = getStorageName(settings);
+    const savedCodeJson = localStorage.getItem(storageName);
+    
+    const languageMode = 'cpp';
+    
+    if (savedCodeJson) {
+        // saved code from db
+        const savedCode = JSON.parse(savedCodeJson);
+        $('.loading.editor').show();
+        loadSample(languageMode, savedCode.mainFile.code);
+        $('.loading.editor').fadeOut({ duration: 300 });
+    }
+    else {
+        // defaul sample
+        initStorageWithTemplateCode(
+            settings,
+            () => {
+                const savedCode = JSON.parse(localStorage.getItem(storageName));
+                $('.loading.editor').show();
+                console.log(savedCode);
+                loadSample(languageMode, savedCode.mainFile.code);
+                $('.loading.editor').fadeOut({ duration: 300 });
+            });
+    }
+    
+    initializeFileHeaders(settings.mainCodeTemplate, settings.codeTemplates);
+
+    window.onresize = function () {
+        if (editor) {
+            editor.layout();
+        }
+        if (diffEditor) {
+            diffEditor.layout();
+        }
+    };
+});
