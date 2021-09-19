b32e58cc3e497921fb30319b98df2f4b03bb4d1e
nemezidait <90568128+nemezidait@users.noreply.github.com>
2021-09-19 15:56:09 +0300

    Update and rename lesson.js to cpplesson.js

---
+++ js/cpplesson.js
@@ -0,0 +1,168 @@
+import { compileGccCpp } from '/wandbox.js';        
+
+"use strict";
+
+var editor = null, diffEditor = null;
+
+function getLessonSettings(){
+    let request = new XMLHttpRequest();
+    request.open("GET", "config.json", false);
+    request.send(null);
+    return JSON.parse(request.responseText);
+}
+
+function getStorageName(settings){
+    return 'cpp_cource_' + settings.lessonId;
+}
+
+function initStorageWithTemplateCode(settings, callback)
+{    
+    const mainCodeURL = settings.codePath + '/' + settings.mainCodeTemplate;
+    // fill main code file
+    xhr(mainCodeURL, function (err, data) {
+            if (err) {
+                onFileLoadError(err);
+                return;
+            }
+            const codeTemplates = {
+                mainFile: {
+                     name: settings.mainCodeTemplate,
+                     code: data,
+                },
+                additionalFiles: []
+            };
+            
+            // load additional code templates
+            loadAdditionalCodeTemplates(
+                settings.codePath,
+                settings.codeTemplates,
+                0,
+                (fileName, code) => codeTemplates.additionalFiles.push({name: fileName, code: code }),
+                () => {
+                     const storageName = getStorageName(settings);
+                     localStorage.setItem(storageName, codeTemplates);
+                },
+                callback   
+            );
+        });
+}
+
+function loadAdditionalCodeTemplates(cadePath, fileNames, currentIdx, onLoaded, onFinished, callback)
+{
+        if (currentIdx >= fileNames.length){
+                onFinished();
+                callback();
+                return;
+        }
+        
+        xhr(cadePath + '/' + fileNames[currentIdx], function (err, data) {
+            if (err) {
+                onFileLoadError(err);
+                return;
+            }
+            onLoaded(fileNames[currentIdx], data);
+            loadAdditionalCodeTemplates(cadePath, fileNames, currentIdx + 1, onLoaded, onFinished, callback);
+        });
+}
+
+function onFileLoadError(err){
+        if (editor) {
+            if (editor.getModel()) {
+                editor.getModel().dispose();
+            }
+            editor.dispose();
+            editor = null;
+        }
+        $('.loading.editor').fadeOut({ duration: 200 });
+        $('#editor').empty();
+        $('#editor').append('<p class="alert alert-error">Failed to load ' + languageCode + ' sample</p>');  
+}
+
+function compile(){
+       
+}
+
+$(document).ready(function () {
+    const settings = getLessonSettings();
+
+    const languageMode = 'cpp';
+        
+    const storageName = getStorageName(settings);
+    const savedCode = localStorage.getItem(storageName);
+        
+    if (savedCode) {
+        // saved code from db
+        $('.loading.editor').show();
+        loadSample(languageMode, savedCode.mainFile.code);
+        $('.loading.editor').fadeOut({ duration: 300 });
+    }
+    else {
+        // defaul sample
+        initStorageWithTemplateCode(
+            settings,
+            () => {
+                const savedCode = localStorage.getItem(storageName);
+                $('.loading.editor').show();
+                loadSample(languageMode, savedCode.mainFile.code);
+                $('.loading.editor').fadeOut({ duration: 300 });
+            });
+    }
+
+    window.onresize = function () {
+        if (editor) {
+            editor.layout();
+        }
+        if (diffEditor) {
+            diffEditor.layout();
+        }
+    };
+});
+
+var preloaded = {};
+(function () {
+    var elements = Array.prototype.slice.call(document.querySelectorAll('pre[data-preload]'), 0);
+
+    elements.forEach(function (el) {
+        var path = el.getAttribute('data-preload');
+        preloaded[path] = el.innerText || el.textContent;
+        el.parentNode.removeChild(el);
+    });
+})();
+
+function xhr(url, cb) {
+    if (preloaded[url]) {
+        return cb(null, preloaded[url]);
+    }
+    $.ajax({
+        type: 'GET',
+        url: url,
+        dataType: 'text',
+        error: function () {
+            cb(this, null);
+        }
+    }).done(function (data) {
+        cb(null, data);
+    });
+}
+
+function loadSample(languageCode, code) {
+    if (!editor) {
+        $('#editor').empty();
+        editor = monaco.editor.create(document.getElementById('editor'), {
+            model: null,
+        });
+    }
+
+    var oldModel = editor.getModel();
+    var newModel = monaco.editor.createModel(code, languageCode);
+    editor.setModel(newModel);
+    if (oldModel) {
+        oldModel.dispose();
+    }
+    changeTheme(0);
+}
+
+function changeTheme(theme) {
+    var newTheme = (theme === 0 ? 'vs-dark' : 'vs');
+    monaco.editor.setTheme(newTheme);
+}
