6de773d71fbffb1a8a551e8c64ec97513e2ae3e0
nemezidait <90568128+nemezidait@users.noreply.github.com>
2021-09-19 18:01:19 +0300

    Update wandbox.js

--- js/wandbox.js
+++ js/wandbox.js
@@ -17,14 +17,10 @@ async function postData(data = {}) {
     // Default options are marked with *
     const response = await fetch('https://wandbox.org/api/compile.json', {
         method: 'POST', // *GET, POST, PUT, DELETE, etc.
-        mode: 'cors', // no-cors, *cors, same-origin
-        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
         headers: {
             'Content-Type': 'application/json'
             // 'Content-Type': 'application/x-www-form-urlencoded',
         },
-        redirect: 'follow', // manual, *follow, error
-        referrerPolicy: 'no-referrer', // no-referrer, *client
         body: data // body data type must match "Content-Type" header
     });
     return await response.json(); // parses JSON response into native JavaScript objects
