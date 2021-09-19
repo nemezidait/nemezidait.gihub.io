42d98c102a1905136ba386d1636ce4f978c9dbee
nemezidait <90568128+nemezidait@users.noreply.github.com>
2021-09-19 18:02:24 +0300

    Update wandbox.js

--- js/wandbox.js
+++ js/wandbox.js
@@ -21,7 +21,7 @@ async function postData(data = {}) {
             'Content-Type': 'application/json'
             // 'Content-Type': 'application/x-www-form-urlencoded',
         },
-        body: data // body data type must match "Content-Type" header
+        body: JSON.stringify(data) // body data type must match "Content-Type" header
     });
     return await response.json(); // parses JSON response into native JavaScript objects
 }
