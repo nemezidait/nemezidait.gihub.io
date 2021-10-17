9c8d731f467dd1cd9d19cad5a3ddc7831c964d71
nemezidait <90568128+nemezidait@users.noreply.github.com>
2021-10-17 23:13:04 +0300

    Update programmingDidciplinesWorker.js

--- js/programmingDidciplinesWorker.js
+++ js/programmingDidciplinesWorker.js
@@ -1,1 +1,44 @@
+function getCoursesSettings() {
+    let request = new XMLHttpRequest();
+    request.open("GET", "disciplinesSettings.json", false);
+    request.send(null);
+    return JSON.parse(request.responseText);
+}
 
+function getDidcipline(courseObj) {
+  let result = '<div class="probootstrap-service-2 probootstrap-animate"><div class="image"><div class="image-bg"><img src="' + courseObj.logo +'"></div></div>';
+  result += '<div class="text"><span class="probootstrap-meta"><i class="icon-calendar2"></i> запущен в 2021 году</span>';
+  result += '<h3>' + courseObj.name + '</h3><p>' + courseObj.description+ '</p>';
+  result += '<p><a href="' + courseObj.path + '" class="btn btn-primary">Начать</a> <span class="enrolled-count">Всего один клик до мечты!</span></p></div>';
+  return result + '</div>';
+}
+
+function getDidciplineBlock(courseObj1, courseObj2 = null) {
+  let result = '<div class="col-md-6">' + getDidcipline(courseObj1);
+  if (courseObj2){
+     result += getDidcipline(courseObj2);
+  }
+  return result + '</div>';
+}
+
+function insertDidciplines(courses) {
+  let length = courses.length;
+  if (courses.length % 2 != 0) {
+    length--;  
+  }
+  let html = '';
+  for(let i = 0; i < length; i += 2){
+    html += getDidciplineBlock(courses[i], courses[i + 1]);
+  }
+  
+  if (length != courses.length) {
+    html += getDidciplineBlock(courses[length]);
+  }
+    
+  document.getElementById("coursesBody").innerHTML = html;
+}
+
+function loadDidciplines(){
+    const settings = getDidciplinesSettings();
+    insertDidciplines(settings.disciplines);
+}
