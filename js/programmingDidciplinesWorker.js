42a3fceb15b34aa81697e51441ce3724c4a8d680
nemezidait <90568128+nemezidait@users.noreply.github.com>
2021-11-22 02:51:38 +0300

    Update programmingDidciplinesWorker.js

--- js/programmingDidciplinesWorker.js
+++ js/programmingDidciplinesWorker.js
@@ -5,34 +5,34 @@ function getDidciplinesSettings() {
     return JSON.parse(request.responseText);
 }
 
-function getDidcipline(courseObj) {
-  let result = '<div class="probootstrap-service-2 probootstrap-animate"><div class="image"><div class="image-bg"><img src="' + courseObj.logo +'"></div></div>';
+function getDidcipline(didciplineObj) {
+  let result = '<div class="probootstrap-service-2 probootstrap-animate"><div class="image"><div class="image-bg"><img src="' + didciplineObj.logo +'"></div></div>';
   result += '<div class="text"><span class="probootstrap-meta"><i class="icon-calendar2"></i> запущен в 2021 году</span>';
-  result += '<h3>' + courseObj.name + '</h3><p>' + courseObj.description+ '</p>';
-  result += '<p><a href="' + courseObj.path + '" class="btn btn-primary">Начать</a> <span class="enrolled-count">Всего один клик до мечты!</span></p></div>';
+  result += '<h3>' + didciplineObj.name + '</h3><p>' + didciplineObj.description+ '</p>';
+  result += '<p><a href="' + didciplineObj.path + '" class="btn btn-primary">Начать</a> <span class="enrolled-count">Всего один клик до мечты!</span></p></div>';
   return result + '</div>';
 }
 
-function getDidciplineBlock(courseObj1, courseObj2 = null) {
-  let result = '<div class="col-md-6">' + getDidcipline(courseObj1);
-  if (courseObj2){
-     result += getDidcipline(courseObj2);
+function getDidciplineBlock(didciplineObj1, didciplineObj2 = null) {
+  let result = '<div class="col-md-6">' + getDidcipline(didciplineObj1);
+  if (didciplineObj2){
+     result += getDidcipline(didciplineObj2);
   }
   return result + '</div>';
 }
 
-function insertDidciplines(courses) {
-  let length = courses.length;
-  if (courses.length % 2 != 0) {
+function insertDidciplines(didciplines) {
+  let length = didciplines.length;
+  if (didciplines.length % 2 != 0) {
     length--;  
   }
   let html = '';
   for(let i = 0; i < length; i += 2){
-    html += getDidciplineBlock(courses[i], courses[i + 1]);
+    html += getDidciplineBlock(didciplines[i], didciplines[i + 1]);
   }
   
-  if (length != courses.length) {
-    html += getDidciplineBlock(courses[length]);
+  if (length != didciplines.length) {
+    html += getDidciplineBlock(didciplines[length]);
   }
     
   document.getElementById("disciplinesBody").innerHTML = html;
