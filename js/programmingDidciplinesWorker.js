ee62128645cceb226b2a18a173b884d25c1835f1
nemezidait <90568128+nemezidait@users.noreply.github.com>
2021-11-22 13:55:41 +0300

    Update programmingDidciplinesWorker.js

--- js/programmingDidciplinesWorker.js
+++ js/programmingDidciplinesWorker.js
@@ -40,5 +40,5 @@ function insertDidciplines(didciplines) {
 
 function loadDidciplines(){
     const settings = getDidciplinesSettings();
-    insertDidciplines(settings.disciplines);
+    insertDidciplines(settings.themes);
 }
