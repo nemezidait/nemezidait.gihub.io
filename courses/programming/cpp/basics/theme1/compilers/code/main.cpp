4994df550d2e0e3bf844f1a5cc73b22027d29b08
nemezidait <90568128+nemezidait@users.noreply.github.com>
2021-12-05 20:01:39 +0300

    Update main.cpp

--- courses/programming/cpp/basics/theme1/compilers/code/main.cpp
+++ courses/programming/cpp/basics/theme1/compilers/code/main.cpp
@@ -5,8 +5,7 @@
  * на MinGW он работает, т.к. MinGW расширяет стандарт C++ таким поведением,
  * а вот msvc (msvcpp) даёт ошибку компиляции.
  */
-int main()
-{
+int main() {
     int length;
     std::cin >> length;
     int array[length];
