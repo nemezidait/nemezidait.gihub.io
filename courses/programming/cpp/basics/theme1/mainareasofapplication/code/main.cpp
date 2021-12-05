7af2ac6fdc6af7242da12198c7c322f1e833c882
nemezidait <90568128+nemezidait@users.noreply.github.com>
2021-12-05 18:19:52 +0300

    Update main.cpp

--- courses/programming/cpp/basics/theme1/mainareasofapplication/code/main.cpp
+++ courses/programming/cpp/basics/theme1/mainareasofapplication/code/main.cpp
@@ -1,5 +1,21 @@
+#include <algorithm>
 #include <iostream>
-
-int main() {
-    std::cout << "Hello World!";
+#include <map>
+#include <set>
+using namespace std;
+ 
+int main()
+{
+	map<string, size_t> countOfWordsMap;
+	for_each(istream_iterator<string>{ cin }, {}, [&countOfWordsMap](const string& word) { ++countOfWordsMap[word]; });
+ 
+	map<size_t, set<string>, greater<size_t>> wordsHierarchy;
+	for_each(countOfWordsMap.begin(), countOfWordsMap.end(), [&wordsHierarchy](const pair<string, size_t>& wordInfo)
+		{ wordsHierarchy[wordInfo.second].insert(wordInfo.first); });
+ 
+	for (const auto& e : wordsHierarchy) {
+		cout << e.first << ": [";
+		copy(e.second.begin(), e.second.end(), ostream_iterator<string>{ cout, "; "});
+		cout << "]\n";
+	}
 }
