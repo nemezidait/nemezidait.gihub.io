28f555eb8be42842c62ba4764f9b27a741d9f65c
nemezidait <90568128+nemezidait@users.noreply.github.com>
2021-12-05 18:21:37 +0300

    Update main.cpp

--- courses/programming/cpp/basics/theme1/mainareasofapplication/code/main.cpp
+++ courses/programming/cpp/basics/theme1/mainareasofapplication/code/main.cpp
@@ -4,14 +4,20 @@
 #include <set>
 using namespace std;
  
-int main()
-{
+int main() {
 	map<string, size_t> countOfWordsMap;
-	for_each(istream_iterator<string>{ cin }, {}, [&countOfWordsMap](const string& word) { ++countOfWordsMap[word]; });
+	for_each(istream_iterator<string>{ cin },
+		 {},
+		 [&countOfWordsMap](const string& word) {
+			 ++countOfWordsMap[word];
+		 });
  
 	map<size_t, set<string>, greater<size_t>> wordsHierarchy;
-	for_each(countOfWordsMap.begin(), countOfWordsMap.end(), [&wordsHierarchy](const pair<string, size_t>& wordInfo)
-		{ wordsHierarchy[wordInfo.second].insert(wordInfo.first); });
+	for_each(countOfWordsMap.begin(),
+		 countOfWordsMap.end(),
+		 [&wordsHierarchy](const pair<string, size_t>& wordInfo) { 
+			 wordsHierarchy[wordInfo.second].insert(wordInfo.first);
+		 });
  
 	for (const auto& e : wordsHierarchy) {
 		cout << e.first << ": [";
