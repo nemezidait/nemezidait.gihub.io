0af04a34e9a5481d0e06bc175921a275cace0cb4
nemezidait <90568128+nemezidait@users.noreply.github.com>
2021-12-18 18:01:04 +0300

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -4,9 +4,7 @@ function showModal(modalId){
     const modal = document.getElementById(modalId); 
     
     const contentWrapper = modal.querySelector('.content-wrapper');
-    const close = modal.querySelector('.close');
-
-    close.addEventListener('click', () => modal.classList.remove('open'));
+    
     modal.addEventListener('click', () => modal.classList.remove('open'));
     contentWrapper.addEventListener('click', (e) => e.stopPropagation());
     
