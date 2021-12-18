0a388907bfe8d61bb888dd68be4ce8fd264728f3
nemezidait <90568128+nemezidait@users.noreply.github.com>
2021-12-18 17:23:34 +0300

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,14 @@
-jQuery(document).ready(function() {
-	
-	"use strict";
-	// Your custom js code goes here.
+"use strict";
 
-});
+function showModal(modalId){
+    const modal = document.getElementById(modalId); 
+    
+    const contentWrapper = modal.querySelector('.content-wrapper');
+    const close = modal.querySelector('.close');
+
+    close.addEventListener('click', () => modal.classList.remove('open'));
+    modal.addEventListener('click', () => modal.classList.remove('open'));
+    contentWrapper.addEventListener('click', (e) => e.stopPropagation());
+    
+    modal.classList.toggle('open');
+}
