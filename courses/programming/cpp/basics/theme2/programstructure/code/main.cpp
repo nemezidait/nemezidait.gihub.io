a36297412bc2eb793bdabf6237af8d0aa44c0159
DDaemonium <linkoln3636@yandex.ru>
2021-12-26 21:47:48 +0300

    Updated

---
+++ courses/programming/cpp/basics/theme2/programstructure/code/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+ 
+/*
+ * Этот код показывает разницу между различными компиляторами:
+ * на MinGW он работает, т.к. MinGW расширяет стандарт C++ таким поведением,
+ * а вот msvc (msvcpp) даёт ошибку компиляции.
+ */
+int main() {
+    int length;
+    std::cin >> length;
+    int array[length];
+}
