8569fa38eb515bc11d1f3f8d3fe3d17ea973d967
andrei ivanov <andrei.ivanov@akvelon.com>
2022-03-15 02:23:52 +0300

    Fixed nav hover

--- js/contentLoader.js
+++ js/contentLoader.js
@@ -10,5 +10,12 @@ async function loadHtmlDocument(path){
 
 document.body.onload = function() {
     loadHtmlDocument("/template/headerTop.html").then(text => document.getElementById('header-top').innerHTML = text);
-    loadHtmlDocument("/template/nav.html").then(text => document.getElementById('nav').innerHTML = text);
+    loadHtmlDocument("/template/nav.html").then(text => document.getElementById('nav').innerHTML = text)
+        .then(_ => {
+            $('.probootstrap-navbar .navbar-nav li.dropdown').hover(function () {
+                $(this).find('> .dropdown-menu').stop(true, true).delay(200).fadeIn(500).addClass('animated-fast fadeInUp');
+            }, function () {
+                $(this).find('> .dropdown-menu').stop(true, true).fadeOut(200).removeClass('animated-fast fadeInUp')
+            });
+        });
 };
