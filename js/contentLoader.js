1a78c3b16df9fdbd5626cee8edd521293ff8b15c
andrei ivanov <andrei.ivanov@akvelon.com>
2022-03-15 02:32:00 +0300

    Fixed nav

--- js/contentLoader.js
+++ js/contentLoader.js
@@ -12,10 +12,12 @@ document.body.onload = function() {
     loadHtmlDocument("/template/headerTop.html").then(text => document.getElementById('header-top').innerHTML = text);
     loadHtmlDocument("/template/nav.html").then(text => document.getElementById('nav').innerHTML = text)
         .then(_ => {
-            $('.probootstrap-navbar .navbar-nav li.dropdown').hover(function () {
-                $(this).find('> .dropdown-menu').stop(true, true).delay(200).fadeIn(500).addClass('animated-fast fadeInUp');
-            }, function () {
-                $(this).find('> .dropdown-menu').stop(true, true).fadeOut(200).removeClass('animated-fast fadeInUp')
-            });
+			if ((window.innerWidth > 800) && (window.innerHeight > 600)) {
+				$('.probootstrap-navbar .navbar-nav li.dropdown').hover(function () {
+					$(this).find('> .dropdown-menu').stop(true, true).delay(200).fadeIn(500).addClass('animated-fast fadeInUp');
+				}, function () {
+					$(this).find('> .dropdown-menu').stop(true, true).fadeOut(200).removeClass('animated-fast fadeInUp')
+				});
+			}
         });
 };
